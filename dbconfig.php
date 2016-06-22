af7194c0316d006f03944c3d7252d205af2310bf
Roopesh <>
2016-06-22 15:13:38 +0530

    e-seventh commit

--- dbconfig.php
+++ dbconfig.php
@@ -1,5 +1,5 @@
   <?php
     $db="test1";//db
     $con=mysql_connect("localhost","root","");
-    mysql_select_db($db,$con);//mysql connectionxxx
+    mysql_select_db($db,$con);//mysql connectionxx
     ?>
